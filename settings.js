8d0f8dd463b4234a124b33f5e2ce6aa37a75d05a
ZannMods <162866065+ZannCyber@users.noreply.github.com>
2024-06-15 22:56:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Zannmods'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Zannmods:o7TzHU2GaODYHJTL@cluster0.snsb4y2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "fauzan.official950@gmail.com" //email
+global.email_password = "Zannmods321 " //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
